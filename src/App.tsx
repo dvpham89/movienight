787e9b338710133099f1811ad6942f0f8ef1b077
Duy Pham <dvpham89@yahoo.com>
2022-05-20 17:35:37 -0400

    blah

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./component/Header";
+import MovieList from "./component/MovieList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <MovieList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/models/Movie.ts
@@ -0,0 +1,7 @@
+export default interface Movie {
+  genre_ids: [];
+  id: number;
+  overview: string;
+  title: string;
+  vote_average: number;
+}
