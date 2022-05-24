33318daef6e2039ca9336887dd18acec038a8bb6
maurice <maurice.mason97@gmail.com>
2022-05-24 11:37:04 -0400

    no

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
-import SearchForm from "./SearchForm";
 
 const Header = () => {
   return (
@@ -8,7 +7,6 @@ const Header = () => {
       <h1>
         <Link to="/">Movie Night</Link>
       </h1>
-
       <p>
         <Link to="/watchList">
           <i className="fa-solid fa-list"> Watch List</i>
